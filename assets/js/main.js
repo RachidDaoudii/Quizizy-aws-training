bcb9d3cdfdfd215b0e01dc99f378918c2fb29d2e
rachiddaoudi <rachiddaoudi533@gmail.com>
2022-12-21 17:33:20 +0100

    fin tache next question

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,108 +1,145 @@
 var checks = document.querySelectorAll(".r");
 var Reponses = document.querySelectorAll('input[type="radio"]');
 let suivant = document.querySelector('.suivant');
-let number = document.querySelector('.TotalQuestion .number');
+let TotalQuestion = document.querySelector('.TotalQuestion .number');
+var indexQuestion = 1;
+var indexReponse=0;
 
-async function getQuestion(){
+//numbre Queqtion
+let numberq = document.querySelector('.Q .number');
+ function getQuestion(){
     $.ajax({
-        url:"/assets/js/data.json",
+        url:"/assets/js/data2.json",
         type:'GEt',
         success:function(data,status){
             let ObjQuestion = new Object(data);
-            // console.log(ObjQuestion.Questions)
-            let countQuestion = ObjQuestion['Questions'].length;
+            let countQuestion = ObjQuestion.length;
             nbr = countQuestion;
-            CountQuestion(nbr)
-            // console.log(ObjQuestion.Questions)
-            AddQuestion(ObjQuestion.Questions,nbr)
+            TotalQuestion.innerHTML = nbr
+            //add first question in page
+            first(ObjQuestion[0])
+            //btn next question
+            suivant.onclick = () =>{
+                let test = ObjQuestion[indexQuestion];
+                reponses(ObjQuestion[indexReponse],nbr)
+                indexQuestion++
+                indexReponse++
+                //vide title question 
+                Questionn.innerHTML='';
+                // vide les reponse
+                document.querySelector(".ul").innerHTML='';
+
+                AddQuestion(test,nbr)
+                
+                CountQuestion(indexQuestion)
+
+                // rt()
+            }            
         }
     })
 }
 getQuestion()
 
 function CountQuestion(nbr){
-    number.innerHTML=nbr;
+    numberq.innerHTML=nbr;
 }
 
+// function rt(){
+//     checks.forEach((check)=>{
+//         check.addEventListener('click', function(){
+//             console.log(check)
+//             Reponses.forEach ((element) =>{
+//                 if (element.value === check.innerText) {
+//                     element.checked = true;
+//                     console.log(element.value)
+//                     // element.style.color= "red";
+//                     // check.style.background="vert";
+//                 }else{
+//                     // check.style.background="red";
+//                 }
+//             })
+//         })
+//     })
+// }
 
 
-checks.forEach((check)=>{
-    check.addEventListener('click', function(){
-        Reponses.forEach ((element) =>{
-            if (element.value === check.innerText) {
-                element.checked = true;
-                console.log(element.value)
-                // element.style.color= "red";
-                // check.style.background="vert";
-            }else{
-                // check.style.background="red";
-            }
-        })
-    })
-})
-
-
+//titile Question
+let Questionn = document.getElementById('Question'); 
+//Reponse
+let re = document.querySelector(".ul");
 
 function AddQuestion(obj,count){
-    //numbre Queqtion
-    let nbr = document.querySelector('.number');
-    //titile Question
-    let Question = document.getElementById('Question'); 
-    //Reponse_1
-    let re = document.querySelector("listReponse");
-    var a=1;
-    for (let i = 0; i < count; i++) {
-        n=1
-        //append numbre
-        nbr.innerText = i+n;
-        //create Question
-        // console.log(obj)
-        if (obj[i]['id'] === a) {
-            let newQuestion = document.createTextNode(obj[i].Question);
-            console.log(newQuestion,a,obj[i]['id']);
-            //Append Question in quiz
-            Question.append(newQuestion);
-            suivant.addEventListener('click',function(){
-                a++
-                console.log(a)
-            })
-        }
-        
-        //create Reponse (radioButton)
-        let radio = document.createElement('input');
+    let newQuestion = document.createTextNode(obj['Question']);
+    //Append Question in quiz
+    Questionn.append(newQuestion);
+
+    for (let i =1;  i<5;i++) {
+        var li = document.createElement("li")
+        let icons = document.createElement('i')
+        let radio = document.createElement('input')
+        let label = document.createElement('label')
+
+        icons.className = 'fa-solid fa-circle-question'
+
         radio.name = 'Reponse'
         radio.type = 'radio'
+        radio.value = obj[`Reponse_${i}`];
         radio.id = `Reponse_${i}`
-        radio.value = obj[`Reponse_${i}`]
-        //create Reponse (a)
-        // if(i === obj['Questions']['id']){
-        //     console.log('TRT')
-        // }
-        // let a = document.createTextNode(obj[i]['Question']);
-        // console.log(a)
-        // re.appendChild(a);
 
-        // console.log(i);
-        // console.log(obj[i]['Question'],obj[i].Reponses[i][`Reponse_1`],obj[i].Reponses[i][`Reponse_2`],obj[i].Reponses[i][`Reponse_3`],obj[i].Reponses[i][`Reponse_4`])
-        
+        label.id= 'idradio'
+        label.className = 'r'
+        label.htmlFor = `Reponse_${i}`
+        label.innerHTML = obj[`Reponse_${i}`]
+
+        li.appendChild(icons)
+        li.appendChild(radio)
+        li.appendChild(label)
+        re.appendChild(li)
     }
 }
 
-// await function suivant(){
-//     let contenu = document.querySelector(".test");
-// }
-// suivant()
-
-
-// function reponseVrais(){
+let checkReponse = document.getElementsByName('Reponse')
+function reponses(obj,nbr){
+    let Reponsess;
+    // test = obj[0]
+    // console.log('question',test)
     
+    
+    for (let i = 0; i<checkReponse.length; i++) {
+        if(checkReponse[i].checked){
+            Reponsess =  checkReponse[i].value
+        }
+    }
+    console.log(' question check est ',Reponsess)
+    // console.log(obj[0].Reponse_vrai)
+    console.log('reponse hhh est',obj['Reponse_vrai'])
+    if(Reponsess === obj['Reponse_vrai']){
+        console.log("good")
+    }else{
+        console.log("not good")
+    }
+}
 
-// }
 
-// function Suivant(){
-//     // var nbr =0
-//     // nbr++
-//     a++;
-//     // number.innerHTML=nbr;
-//     console.log(a)
-// }
+function first(t){
+    Question = t.Question
+    Reponse_1 = t.Reponse_1
+    Reponse_2 = t.Reponse_2
+    Reponse_3 = t.Reponse_3
+    Reponse_4 = t.Reponse_4
+    Reponse_vrai = t.Reponse_vrai
+    document.querySelector('#Question').innerText=Question
+   document.querySelector('#Reponse_1').value=Reponse_1
+   document.querySelector('#Reponse_2').value=Reponse_2
+   document.querySelector('#Reponse_3').value=Reponse_3
+   document.querySelector('#Reponse_4').value=Reponse_4
+   document.querySelector('.ul').children[0].children[2].innerText=Reponse_1
+   document.querySelector('.ul').children[1].children[2].innerText=Reponse_2
+   document.querySelector('.ul').children[2].children[2].innerText=Reponse_3
+   document.querySelector('.ul').children[3].children[2].innerText=Reponse_4
+}
+
+
+
+
+
